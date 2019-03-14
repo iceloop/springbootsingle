a09f9119d844dca8c9931d5b13514a725bf8e172
iceloop <870285972@qq.com>
2019-03-14 14:32:45 +0800

    fgbhfgf

--- springboot/src/main/java/com/example/springboot/controller/helloController.java
+++ springboot/src/main/java/com/example/springboot/controller/helloController.java
@@ -12,6 +12,7 @@ public class helloController {
     public String hello(){
         System.out.println("fenzhi");
         System.out.println("hello world");
+        System.out.println("hello worldasd");
         return "HelloWord";
     }
 }
